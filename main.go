0b62710a77da9cfac231092e1f6087025653c5d3
João Gabriel <jg7607221@gmail.com>
2025-10-24 17:08:06 -0300

    add comment 24/out

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"consultancy_hours/controllers"
 	"fmt"
 	"net/http"
+	//	"go.mongodb.org/mongo-driver/v2/mongo"
+	//	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	//	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func main() {
+
+	//options.Client().ApplyURI("mongodb+srv://bagual:<db_password>@agenda.ipvs0d5.mongodb.net/?appName=agenda").SetServerAPIOptions(serverAPI)
 
 	http.HandleFunc("/horarios/disponiveis", controllers.ConsultHandler)
 	http.HandleFunc("/agendar", controllers.ScheduleHandler)
