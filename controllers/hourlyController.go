de09973ed15cf6217b620c3917825ccf0ff887f6
gabrielbagual <jg7607221@gmail.com>
2025-10-25 12:52:37 -0300

    DB create + integration + logic

--- controllers/hourlyController.go
+++ controllers/hourlyController.go
@@ -1,23 +1,36 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type Controller struct {
+	Db *mongo.Database
+}
+
+func NewController(db *mongo.Database) *Controller {
+	return &Controller{Db: db}
+}
 
 type OrderMark struct {
 	IDHorario   string `json:"id_horario"`
 	NomeCliente string `json:"nome_cliente"`
 }
 
-func ConsultHandler(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) ConsultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"mensagem": "Hello World Consult"})
+
+	json.NewEncoder(w).Encode(map[string]string{"mensagem": "Hello World Consult - Em breve consultando o DB"})
 }
 
-func ScheduleHandler(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) ScheduleHandler(w http.ResponseWriter, r *http.Request) {
 	var pedido OrderMark
 	err := json.NewDecoder(r.Body).Decode(&pedido)
 
@@ -30,8 +43,22 @@ func ScheduleHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Recebido pedido para o horario %s do cliente %s\n", pedido.IDHorario, pedido.NomeCliente)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	collection := c.Db.Collection("agendamentos")
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	res, err := collection.InsertOne(ctx, pedido)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"erro": "Falha ao agendar no banco de dados"})
+		return
+	}
+
+	fmt.Printf("Inserido no DB com ID: %v\n", res.InsertedID)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // StatusOK (200) ou StatusCreated (201)
 	json.NewEncoder(w).Encode(map[string]string{"mensagem": "Agendamento recebido com sucesso!"})
 }
--- main.go
+++ main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
-	"consultancy_hours/controllers"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	//	"go.mongodb.org/mongo-driver/v2/mongo"
-	//	"go.mongodb.org/mongo-driver/v2/mongo/options"
-	//	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+	"os"
+
+	"consultancy_hours/controllers"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-
-	//options.Client().ApplyURI("mongodb+srv://bagual:<db_password>@agenda.ipvs0d5.mongodb.net/?appName=agenda").SetServerAPIOptions(serverAPI)
-
-	http.HandleFunc("/horarios/disponiveis", controllers.ConsultHandler)
-	http.HandleFunc("/agendar", controllers.ScheduleHandler)
-
-	fmt.Println("Initing on port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error strating server ", err)
+	if err := godotenv.Load(); err != nil {
+		log.Println("Arquivo .env não encontrado, usando variáveis de ambiente")
 	}
 
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI não definida no .env")
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Erro ao conectar ao MongoDB: ", err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Erro ao pingar o MongoDB: ", err)
+	}
+
+	fmt.Println("Conectado ao MongoDB!")
+
+	db := client.Database("agenda")
+
+	appController := controllers.NewController(db)
+
+	http.HandleFunc("/horarios/disponiveis", appController.ConsultHandler)
+	http.HandleFunc("/agendar", appController.ScheduleHandler)
+
+	fmt.Println("Iniciando servidor na porta 8080...")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Erro ao iniciar servidor: ", err)
+	}
 }
 
 //curl -X POST http://localhost:8080/agendar \
